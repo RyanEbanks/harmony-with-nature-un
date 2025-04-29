ce6ada6f493db71227aea5102102a587ac6d23b2
Ryan Ebanks <ryankebanks@gmail.com>
2025-04-29 16:50:38 -0400

    More home page styling changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,16 +9,16 @@ function Home() {
   const videoContainerRef = useRef<HTMLDivElement | null>(null);
 
   function scrollLeft() {
-    videoContainerRef.current?.scrollBy({ left: -200, behavior: "smooth" });
+    videoContainerRef.current?.scrollBy({ left: -200, behavior: 'smooth' });
   }
 
   function scrollRight() {
-    videoContainerRef.current?.scrollBy({ left: 200, behavior: "smooth" });
+    videoContainerRef.current?.scrollBy({ left: 200, behavior: 'smooth' });
   }
 
 
   const carousel = ytVideos.map((item) => (
-    <div className="home-link-container overflow-hidden">
+    <div className='home-link-container overflow-hidden'>
       <a href={`${item.link}`} target='_blank' rel='noopener noreferrer' className='block'>
         <img
           src={`/images/${item.source}.jpg`}
@@ -35,26 +35,25 @@ function Home() {
 
   return (
     <div className='home-container'>
-      <div className="thumbnail-parent-container">
+      <div className='thumbnail-parent-container'>
         <div ref={videoContainerRef} className='home-video-container mask-gradient'>
-        {carousel}
+          {carousel}
         </div>
-        <div className="button-scroll">
+        <div className='button-scroll'>
           <button onClick={scrollLeft} className=''>
-            <img src="/images/Chevron_Left.svg" alt="left arrow" className='w-12 h-12' />
+            <img src='/images/Chevron_Left.svg' alt='left arrow' className='w-12 h-12' />
           </button>
           <button onClick={scrollRight} className=''>
-            <img src="/images/Chevron_Right.svg" alt="right arrow" className='w-12 h-12' />
+            <img src='/images/Chevron_Right.svg' alt='right arrow' className='w-12 h-12' />
           </button>
         </div>
       </div>
       {/* old bg for div on line 10: bg-[#afb482] bg-opacity-80 */}
-      <div className='flex flex-col lg:flex-row justify-center pt-10 pb-10 overflow-hidden max-w-full'>
+      <div className='flex flex-col lg:flex-row justify-center pt-10 pb-10 overflow-hidden max-w-full mt-10'>
 
         <div className='text-lg md:text-2xl lg:w-[80%] flex flex-col font-normal text-center md:text-left lg:my-auto mx-5 md:mx-8 pt-5 lg:pt-0 px-5 lg:px-[50px]'>
-          <div className='ml-auto my-20 text-end text-2xl md:text-5xl font-light border-r-8 border-[#F0910c] w-full lg:w-1/2 pr-4'>
-            <h1 className=''>HARMONY WITH</h1>
-            <h1 className=''>NATURE</h1>
+          <div className='mx-auto mb-5 text-center text-2xl md:text-5xl font-light w-full lg:w-1/2 pr-4'>
+            <h1 className=''>HARMONY WITH NATURE</h1>
           </div>
           <p className='font-light leading-golden lg:leading-golden-lg'>
             The Harmony with Nature Knowledge Network is an online platform uniting practitioners, academics, and researchers committed to promoting an Earth-centered worldview known as Earth Jurisprudence.
@@ -63,30 +62,35 @@ function Home() {
           </p>
         </div>
       </div>
-      <div className='w-full lg:w-[80%] lg:mx-auto '>
-        <div className='flex flex-col lg:flex-row mt-[160px] pb-5 mb-[38px]'>
-          <div className='pr-8 flex flex-col lg:mr-5 w-1/2'>
-            <div className='text-center lg:text-left text-2xl md:text-5xl font-light mx-8 lg:mx-0 border-b-[6px] border-[#40916C] pb-2 mb-[27px] w-[85%]'>
-              <h1>International Mother</h1>
-              <h1>Earth Day</h1>
+
+      <div className='w-full lg:w-[80%] lg:mx-auto'>
+        <div className='home-content-parent-container my-40'>
+          <div className="home-content-child-container flex flex-col lg:flex-row ">
+            <div className='home-image flex-1'>
+              <img className='home-picture-styling mx-auto lg:mr-auto lg:ml-0 mb-5 lg:mb-0' alt='United Nations Hall 2009 Personnel' src='/images/UN2009.jpg'></img>
             </div>
-            <img className='lg:hidden flex mx-auto lg:mx-0 home-picture-styling lg:ml-0 mb-10 lg:mb-0' alt='United Nations Hall 2009' src='/images/UN2009.jpg'></img>
-            <p className='text-center lg:text-left text-xl font-normal mx-8 md:mx-auto lg:mx-0 leading-golden lg:leading-golden-lg'>In 2009, the United Nations established International Mother Earth Day to promote Harmony with Nature and address the urgent need for sustainable
-              practices. This initiative emphasizes the importance of balancing economic, social, and environmental needs to safeguard our planet and future generations.
-            </p>
-            <button disabled className='btn-primary home-btn-styling mx-auto lg:ml-0 cursor-not-allowed'>Learn More</button>
+            <div className='home-content flex-1'>
+              <h1 className='mx-auto lg:mx-0 w-3/4 text-center lg:text-left text-2xl md:text-5xl font-light border-b-[6px] border-[#40916C] pb-2 mb-[27px]'>International Mother Earth Day</h1>
+              <p className='md:w-3/4 lg:w-full text-center lg:text-left text-sm md:text-lg lg:text-xl font-normal mx-8 md:mx-auto lg:mx-0 leading-golden lg:leading-golden-lg'>In 2009, the United Nations established International Mother Earth Day to promote Harmony with Nature and address the urgent need for sustainable
+                practices. This initiative emphasizes the importance of balancing economic, social, and environmental needs to safeguard our planet and future generations.
+              </p>
+              <button disabled className='hidden lg:block btn-primary home-btn-styling cursor-not-allowed'>Learn More</button>
+            </div>
           </div>
-          <img className='hidden lg:flex home-picture-styling ml-8 lg:ml-0 mb-10 lg:mb-0 w-[40%]' alt='United Nations Hall 2009' src='/images/UN2009.jpg'></img>
         </div>
-        <div className='flex flex-col lg:flex-row mt-[160px] pb-5 mb-20'>
-          <img className='home-picture-styling hidden lg:block' alt='United Nations Hall 2009 Personnel' src='/images/UN2009-2.jpg'></img>
-          <div className='pl-8 flex flex-col lg:ml-5'>
-            <h1 className='text-center lg:text-left text-2xl md:text-5xl font-light mx-8 lg:mx-0 border-b-[6px] border-[#F0910c] pb-2 mb-[27px] w-[85%]'>Nature as a Global Entity</h1>
-            <img className='lg:hidden flex mx-auto lg:mx-0 home-picture-styling lg:ml-0 mb-10 lg:mb-0' alt='United Nations Hall 2009 Personnel' src='/images/UN2009-2.jpg'></img>
-            <p className='text-center lg:text-left text-xl font-normal mx-8 lg:mx-0 leading-golden lg:leading-golden-lg'>The UN’s resolutions advocate for a non-anthropocentric relationship with Nature, respecting Earth as a living entity vital to human survival rather
-              than a commodity. These initiatives emphasize sustainability by preserving Earth's resources and integrating the rights of Nature into global development efforts, promoting shared responsibility across cultures to protect the planet.
-            </p>
-            <button disabled className='btn-secondary home-btn-styling mx-auto lg:ml-0 cursor-not-allowed'>Learn More</button>
+
+        <div className='home-content-parent-container mb-40'>
+          <div className="home-content-child-container flex flex-col-reverse lg:flex-row ">
+            <div className='home-content flex-1'>
+              <h1 className='mx-auto lg:mx-0 w-3/4 text-center lg:text-left text-2xl md:text-5xl font-light border-b-[6px] border-[#F0910c] pb-2 mb-[27px]'>Nature as a Global Entity</h1>
+              <p className='md:w-3/4 lg:w-full text-center lg:text-left text-sm md:text-lg lg:text-xl font-normal mx-8 md:mx-auto lg:mx-0 leading-golden lg:leading-golden-lg'>The UN’s resolutions advocate for a non-anthropocentric relationship with Nature, respecting Earth as a living entity vital to human survival rather
+                than a commodity. These initiatives emphasize sustainability by preserving Earth's resources and integrating the rights of Nature into global development efforts, promoting shared responsibility across cultures to protect the planet.
+              </p>
+              <button disabled className='hidden lg:block btn-secondary home-btn-styling cursor-not-allowed'>Learn More</button>
+            </div>
+            <div className='home-image flex-1'>
+              <img className='home-picture-styling mx-auto lg:ml-auto lg:mr-0 mb-5 lg:mb-0' alt='United Nations Hall 2009 Personnel' src='/images/UN2009-2.jpg'></img>
+            </div>
           </div>
         </div>
       </div>
